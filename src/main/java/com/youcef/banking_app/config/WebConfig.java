402017ccec282f1d283c4eda3477fd576e92333e
youcef <ykesbi@gmail.com>
2026-05-18 17:07:41 +0300

    configuring vercel link

--- src/main/java/com/youcef/banking_app/config/WebConfig.java
+++ src/main/java/com/youcef/banking_app/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://banking-frontend-red.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
